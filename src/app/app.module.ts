ec1c2ceb000925d76caf6e30eb8487f691de5597
moesansigolo <moises.sansigolo@nava.com.br>
2022-10-03 19:01:26 -0300

    adicionando bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from'@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { ProdutosComponent } from './produtos/produtos.component';
 import { FornecedoresComponent } from './fornecedores/fornecedores.component';
@@ -16,7 +17,8 @@ import { FornecedoresComponent } from './fornecedores/fornecedores.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
